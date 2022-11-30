e2aa9acf4451852ac74e36a5ab26a83e0f6f5763
felipe <yfsegura04@misena.edu.co>
2022-11-30 12:29:24 -0500

    primera version de migracion del proyecto

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,167 @@
+<template>
+  <section>
+  <div class="container position-relative">
+    <div class="row align-items-center py-8">
+      <div class="col-md-5 col-lg-6 order-md-1 text-center"><img class="img-fluid"
+          src="../assets/movil-inicio.png" style="max-width:270px ;" alt="" /></div>
+      <div class="col-md-7 col-lg-6 text-center text-md-start"><span
+          class="badge bg-light rounded-pill text-dark align-items-center d-flex flex-row-reverse justify-content-end mx-auto mx-md-0 ps-0 w-75 w-sm-50 w-md-75 w-xl-50 mb-3">
+          APP #1
+          Transporte De Carga 2023<img class="img-fluid float-start me-3" src="assets/img/illustrations/arrow-right.png"
+            alt="" /></span>
+        <h1 class="mb-4 display-3 fw-bold lh-sm">TRAMO <br class="d-block d-lg-none d-xl-block d5" />Transporte y
+          Movilidad</h1>
+        <p class="mt-3 mb-4 fs-1">Solicita tu servicio desde cualquier lugar <br class="d-none d-lg-block" />Descargando
+          nuestra aplicación, disponible para Android y IOS</p>
+        <div class="text-center text-md-start">
+          <a class="btn btn-lg btn-success rounded-pill hover-top" href="#descarga-con">Descarga Gratis</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+  
+  
+  
+  <!-- ============================================-->
+  <!-- <section> begin ============================-->
+  <section class="py-7">
+  
+    <div class="container">
+      <div class="row">
+        <div class="col-12 mx-auto align-items-center text-center">
+          <p class="mb-4">Empresas que trabajan con nosotros</p>
+        </div>
+      </div>
+      <div class="row align-items-center justify-content-center justify-content-lg-around">
+        <div class="col-6 col-sm-4 col-md-4 col-lg-2 px-md-0 mb-5 mb-lg-0 text-center"><img
+            src="../assets/gallery/company-1.png" alt="" /></div>
+        <div class="col-6 col-sm-4 col-md-4 col-lg-2 px-md-0 mb-5 mb-lg-0 text-center"><img
+            src="../assets/gallery/company-2.png" alt="" /></div>
+        <div class="col-6 col-sm-4 col-md-4 col-lg-2 px-md-0 mb-5 mb-lg-0 text-center"><img
+            src="../assets/gallery/company-3.png" alt="" /></div>
+        <div class="col-6 col-sm-4 col-md-4 col-lg-2 px-md-0 mb-5 mb-lg-0 text-center"><img
+            src="../assets/gallery/company-4.png" alt="" /></div>
+        <div class="col-6 col-sm-4 col-md-4 col-lg-2 px-md-0 mb-5 mb-lg-0 text-center"><img
+            src="../assets/gallery/company-1.png" alt="" /></div>
+      </div>
+    </div>
+  
+  </section>
+  <!-- <section> close ============================-->
+  <!-- ============================================-->
+  
+  
+  
+  <!-- <section> close ============================-->
+  <!-- ============================================-->
+  
+  
+  <section class="py-5" id="features">
+    <div class="container-lg">
+      <div class="row align-items-center">
+        <div class="col-md-4 col-lg-5 order-md-0 text-center text-md-start mb-5 mb-md-0"><img class="img-fluid"
+            src="assets/img/gallery/movil-cliente.png" style="max-width:270px ;" alt="" /></div>
+        <div class="col-md-8 col-lg-7 px-sm-5 px-md-0">
+          <h6 class="fw-bold fs-4 display-3 lh-sm">Caracteristicas de la<br />App TRAMO</h6>
+          <p class="my-4">TRAMO conecta el campo y las ciudades en una sola aplicación,<br
+              class="d-none d-xl-block" />siendo la primera empresa con enfoque en el campo.</p>
+          <div class="d-flex align-items-center mb-5">
+            <div><img class="img-fluid" src="assets/img/illustrations/fast-performance.png" width="90" alt="" /></div>
+            <div class="px-4">
+              <h5 class="fw-bold text-danger">Servicios Rapidos</h5>
+              <p>Encuentra los vehiculos disponibles que esten <br class="d-none d-xl-block"> más cerca de tu
+                ubicación.</p>
+            </div>
+          </div>
+          <div class="d-flex align-items-center mb-5">
+            <div><img class="img-fluid" src="assets/img/illustrations/prototype.png" width="90" alt="" /></div>
+            <div class="px-4">
+              <h5 class="fw-bold text-primary">Ahorro</h5>
+              <p>Podrás ahorrar en tiempo y precios reduciendo <br class="d-none d-xl-block">y controlando los
+                sobreprecios en los servicios<br class="d-none d-xl-block"> de transporte</p>
+            </div>
+          </div>
+          <div class="d-flex align-items-center mb-5">
+            <div><img class="img-fluid" src="assets/img/illustrations/vector.png" width="90" alt="" /></div>
+            <div class="px-4">
+              <h5 class="fw-bold text-success">Desarrollo Para El Campo</h5>
+              <p>Ofrecemos cobertura en en zonas urbanas y rurales<br class="d-none d-xl-block">para sacarte de apuros
+                en cualquier condición.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  <!-- ============================================-->
+  <!-- <section> begin ============================-->
+  <section class="py-6">
+  
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-6 text-center text-md-start offset-md-1">
+          <h6 class="fw-bold fs-4 display-3 lh-sm">Acerca de Nosotros</h6>
+          <p class="my-4 pe-xl-5"> TRAMO es una aplicación que conecta de forma más fácil, rapida y segura a las
+            personas que requieren de un servicio de transporte de carga para sus productos. <br
+              class="d-none d-xl-block"><br> No solamente con enfoque en las ciudades, sino que también en el campo,
+            debido a que en el departamento del Cauca cerca del 60% de la población pertenece a zonas rurales.</p>
+          <a class="btn btn-lg btn-primary rounded-pill hover-top mb-5 mb-lg-0" href="#" role="button">Ver Más</a>
+        </div>
+        <div class="col-md-5 order-md-0 text-center text-md-start"><img class="img-fluid mb-4"
+            src="assets/img/illustrations/acerca_nosotros_ilus.svg" width="300" alt="" />
+        </div>
+  
+      </div>
+    </div>
+    <!-- end of .container-->
+  
+  </section>
+  <!-- <section> close ============================-->
+  <!-- ============================================-->
+  
+  <!-- ============================================-->
+  <!-- <section> begin ============================-->
+  
+  
+    
+    <!-- end of .container-->
+  
+ 
+  <!-- <section> Descarga ============================-->
+  <!-- ============================================-->
+  
+  
+  <section class="py-6" id="descarga-con">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-5 col-lg-7 order-md-1 text-center z-index-2 cta-image">
+          <img class="img-fluid mb-4 mb-md-0 img_descarga" src="../assets/illustrations/cta.svg" width="400px" alt="" />
+        </div>
+        <div class="col-md-7 col-lg-5 text-center text-md-start">
+          <h1 class="display-3 fw-bold lh-sm mt-md-8">Descarga La App De Tramo</h1>
+          <p class="my-4"> The rise of mobile devices transforms the way we consume information entirely and the
+            world's most elevant channels such as Facebook.</p>
+          <div class="d-flex justify-content-center d-md-inline-block"><a class="pe-2 pe-sm-3 pe-md-4" href="!#"><img
+                src="assets/img/illustrations/google-play.png" width="160" alt="" /></a><a href="!#"><img
+                src="assets/img/illustrations/app-store.png" width="160" alt="" /></a></div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+<nav-component></nav-component>
+<HelloWorld></HelloWorld>
+<footer-component></footer-component>
+</template>
+
+<script>
+import NavComponent from '@/components/NavComponent.vue'
+
+import HelloWorld from '../components/HelloWorld.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld,
+    NavComponent,
+    FooterComponent
+  }
+}
+</script>
